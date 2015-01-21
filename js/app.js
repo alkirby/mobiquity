ed0bdf21e0605aec6e1cd0bc287e23fba945f116
Al Kirby <al.kirby@gmail.com>
2015-01-21 15:57:52 -0500

    Static mobile list of calendar events.

--- js/app.js
+++ js/app.js
@@ -1,33 +1,3 @@
-var source = $("#calendar").html(); 
-var template = Handlebars.compile(source); 
-
-var data = { 
-    users: [ { 
-        person: {
-            firstName: "Garry", 
-            lastName: "Finch"
-        },
-        jobTitle: "Front End Technical Lead",
-        twitter: "gazraa" 
-    }, {
-        person: {
-            firstName: "Garry", 
-            lastName: "Finch"
-        }, 
-        jobTitle: "Photographer",
-        twitter: "photobasics"
-    }, {
-        person: {
-            firstName: "Garry", 
-            lastName: "Finch"
-        }, 
-        jobTitle: "LEGO Geek",
-        twitter: "minifigures"
-    } ]
-}; 
-
-Handlebars.registerHelper('fullName', function(person) {
-  return person.firstName + " " + person.lastName;
+$(function () {
+	$('#datetimepicker1').datetimepicker({ 'format' : 'M/D/YYYY' });
 });
-
-$('body').append(template(data));
